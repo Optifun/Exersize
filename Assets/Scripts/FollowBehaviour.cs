18c83781f351cedc1a65a87c449a762a011bd2cf
Optifun <frdima19@gmail.com>
2022-08-19 16:03:04 +0700

    Add FollowBehaviour

---
+++ Assets/Scripts/FollowBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class FollowBehaviour : MonoBehaviour
+    {
+        public Transform Target;
+
+        public Vector2 Boundary;
+
+        public void Follow(Transform target)
+        {
+            Target = target;
+        }
+
+        private void Update()
+        {
+            if (Target == null)
+                return;
+
+            Vector3 delta = Target.position - transform.position;
+
+            if (Mathf.Abs(delta.x) >= Boundary.x || Mathf.Abs(delta.y) >= Boundary.y)
+                transform.position = transform.position + delta * Time.deltaTime;
+        }
+
+        private void OnDrawGizmos()
+        {
+            var hOffset = new Vector3(Boundary.x, 0, 0);
+            var vOffset = new Vector3(0, Boundary.y, 0);
+            Vector3 center = transform.position;
+
+            var defaultColor = Gizmos.color;
+            Gizmos.color = Color.green;
+
+            // right up -> right down
+            Gizmos.DrawLine(center + hOffset + vOffset, center + hOffset - vOffset);
+            Gizmos.DrawLine(center - hOffset - vOffset, center + hOffset - vOffset);
+            Gizmos.DrawLine(center - hOffset - vOffset, center - hOffset + vOffset);
+            Gizmos.DrawLine(center - hOffset + vOffset, center + hOffset + vOffset);
+            Gizmos.color = defaultColor;
+        }
+    }
+}
