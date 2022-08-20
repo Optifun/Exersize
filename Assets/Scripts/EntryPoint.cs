48cd41d901c65cef82c7e67671f78672457c950a
Optifun <frdima19@gmail.com>
2022-08-20 17:20:48 +0700

    Update scene & entrypoint

--- Assets/Scripts/EntryPoint.cs
+++ Assets/Scripts/EntryPoint.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
+using Player;
 using UnityEngine;
 
 [DefaultExecutionOrder(-100)]
 public class EntryPoint : MonoBehaviour
 {
+    [SerializeField] private GameObject PlayerPrefab;
+    [SerializeField] private PlayerProps PlayerProps;
 
-	private void Awake ()
-	{
-		
-	}
-	// Start is called before the first frame update
-	private void Start()
+    private GameObject player;
+
+    private void Awake()
     {
-        
+        player = GameObject.Instantiate(PlayerPrefab);
+        player.GetComponent<HealthComponent>().Initialize(PlayerProps.HealthPoints);
     }
 }
