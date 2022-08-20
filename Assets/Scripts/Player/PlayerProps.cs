4548fc760f6998bc2910b8998bb091a6a47da243
Optifun <frdima19@gmail.com>
2022-08-20 17:20:30 +0700

    Add PlayerProps SO

---
+++ Assets/Scripts/Player/PlayerProps.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player properties")]
+public class PlayerProps : ScriptableObject
+{
+    public float HealthPoints;
+}
